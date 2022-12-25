819b7bff09a5109fcb633f213c2c9cdc35250482
alpha-hexor <itsalexwilliams69@gmail.com>
2022-12-24 23:01:40 -0800

    fixed i guess

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from parse_output import *
 
 import subprocess as sp
-url = "https://www.youtube.com/watch?v=qQIKlRpwSbU"
+url = "https://www.youtube.com/watch?v=-2y3Gp8X-Us"
 result =  sp.run(['yt-dlp','-q','-F', url],capture_output=True).stdout.decode()
 ids,ext,res,size,formats=parse(result)
